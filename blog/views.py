b40497366dbf5cb2fdab96655ca5105b9e5e1cab
patrickDugan04 <patrickdugan04.com>
2020-07-24 11:12:58 -0400

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.contrib.auth.models import User
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter()
+    return render(request, 'blog/post_list.html', {'posts': posts})
